9786ff94e52abd6097b57af61e27c48df74ec8e8
peterdillon <peter.dillon.biz@gmail.com>
2025-01-13 11:21:35 -0800

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal, computed } from '@angular/core';
+import { DynamoDBService } from './dynamoDB.service';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+export interface Data {
+  id: string;
+  name: string;
+  price: string;
+};
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'temp';
+
+  title = 'Product Manager';
+  data = signal<Data[]>([]);
+  createProductForm!: FormGroup;
+  
+  constructor( 
+    private dbService: DynamoDBService,
+    private fb: FormBuilder ) { }
+    
+
+  ngOnInit() {
+    this.initForm();
+   }
+
+   initForm() {
+    this.createProductForm = this.fb.group({
+      name: [''],
+      id: [''],
+      price: ['']
+    });
+   }
+
+  saveNewItem() {
+    this.dbService.saveData(
+      this.createProductForm.get('id')?.value,
+      this.createProductForm.get('price')?.value,
+      this.createProductForm.get('name')?.value
+    );
+  }
+
+  getData() {
+    this.dbService.getData()
+    .subscribe(data => { 
+      console.info(data);
+      data = this.data.set(data);
+    });
+  }
+
 }
---
+++ src/app/dynamoDB.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, signal } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DynamoDBService {
+  url = 'https://eky3iequs9.execute-api.us-east-1.amazonaws.com/items';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+    })
+  };
+
+  constructor( private http: HttpClient ) { }
+
+  getData(): Observable<any> {
+    return this.http.get(this.url);
+  }
+
+  saveData(id: string, price: string, name: string) {
+    const body = {id: id, price: price, name: name};
+    this.http.put(this.url, body, this.httpOptions)
+        .subscribe(res => console.log(res));
+  }
+}
